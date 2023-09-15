ca98f16725b8e2cec1d18b8f147f1b1f87f426b6
NazmulHasan000 <nazmulhasanshadin000@gmail.com>
2023-09-15 12:44:49 +0600

    title and alert system updated

--- src/Components/Course/Course.jsx
+++ src/Components/Course/Course.jsx
@@ -1,6 +1,7 @@
 
 import { FaBook, FaDollarSign } from 'react-icons/fa';
 
+
 const Course = ({course,handleAddToCart,toast}) => {
    
      const {img,details,courseName,pirce,credit}=course;
@@ -11,17 +12,19 @@ const Course = ({course,handleAddToCart,toast}) => {
   <figure className=" p-2 ">
     <img  src={img} alt="Shoes" className="rounded-xl w-full" />
   </figure>
-  <div className="p-4 items-center text-center space-y-2">
-    <h2 className="card-title font-semibold  ">{courseName}</h2>
-    <p className='text-[#1c1b1b] font-normal text-justify tracking-tight'>{details}</p>
+  <div className=" items-center text-center space-y-2">
+    <h2 className="card-title font-semibold px-2 ">{courseName}</h2>
+<div className='p-4 space-y-2'>
+<p className='text-[#1c1b1b] font-normal text-justify tracking-tight'>{details}</p>
     <div className='flex justify-between gap-2'>
      <p className='flex items-center font-medium gap-1'>  <FaDollarSign/>    <span> Price:{pirce}  </span> </p>
   <p className='flex items-center font-medium gap-1'>   <FaBook/>   <span className='font-medium'>Credit:{credit}</span></p>
     </div>
     <div className="card-actions w-[100%] ">
-      <button onClick={()=>handleAddToCart(course)} className="btn bg-[#2F80ED]   w-full text-white">Select</button>
+      <button onClick={()=>handleAddToCart(course)} className="btn bg-[#2F80ED]   w-full text-white">Select</button> 
    
     </div>
+</div>
   </div>
 </div>
         </div>
